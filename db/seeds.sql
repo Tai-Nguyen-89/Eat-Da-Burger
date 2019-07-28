0991aa3ace5a35a44cac126373f370d66359ca25
Tai <taing89@gmail.com>
2019-07-28 12:18:17 -0700

    Update: No seeds.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,8 @@
-INSERT INTO burgers (name) VALUES ('Meeses');
-INSERT INTO burgers (name) VALUES ('Bobbi');
-INSERT INTO burgers (name, eatBurger) VALUES ('Sylvester', true);
-INSERT INTO burgers (name, eatBurger) VALUES ('Marilyn Manson', true);
-INSERT INTO burgers (name, eatBurger) VALUES ('Joe Biden', true);
-INSERT INTO burgers (name) VALUES ('Owens');
+-- Seeds not needed for this HW.
+
+-- INSERT INTO burgers (name) VALUES ('Meeses');
+-- INSERT INTO burgers (name) VALUES ('Bobbi');
+-- INSERT INTO burgers (name, eatBurger) VALUES ('Sylvester', true);
+-- INSERT INTO burgers (name, eatBurger) VALUES ('Marilyn Manson', true);
+-- INSERT INTO burgers (name, eatBurger) VALUES ('Joe Biden', true);
+-- INSERT INTO burgers (name) VALUES ('Owens');
